ef6a334ed7982fb25172253717f69d7e0111b9f0
amirShahin-7 <amirshahin.1.512@gmail.com>
2026-02-11 14:50:04 +0200

    add helper to retrieve user access token from NextAuth session.

--- src/helpers/getUserToken.ts
+++ src/helpers/getUserToken.ts
@@ -2,7 +2,13 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function getUserToken() {
-  const sessionToken = (await cookies()).get("next-auth.session-token")?.value;
+  const cookieStore = await cookies();
+  const sessionToken =
+    cookieStore.get("__Secure-next-auth.session-token")?.value ||
+    cookieStore.get("next-auth.session-token")?.value;
+
+  if (!sessionToken) return null;
+
   const accessToken = await decode({
     token: sessionToken,
     secret: process.env.AUTH_SECRET!,
